49ed9a6fd23487e40aab6e51829698b124c83691
yongxin.mo <yongxin.mo@shopee.com>
2019-03-13 14:07:08 +0800

    add: koa-demo

--- koa-demo/server.js
+++ koa-demo/server.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const app = new Koa();
 
+/*
 // logger
 
 app.use(async (ctx, next) => {
@@ -22,6 +23,26 @@ app.use(async (ctx, next) => {
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
+});
+*/
+
+app.use(async function (ctx, next) {
+  console.log('>> one');
+  await next();
+  console.log('<< one');
+});
+
+app.use(async function (ctx, next) {
+  console.log('>> two');
+  ctx.body = 'two';
+  await next();
+  console.log('<< two');
+});
+
+app.use(async function (ctx, next) {
+  console.log('>> three');
+  await next();
+  console.log('<< three');
 });
 
 app.listen(3001);
