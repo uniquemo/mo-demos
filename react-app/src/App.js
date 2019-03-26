8aef9e7d188a7b26d7617fd7d09d697081133c2a
moyongxin <moyongxin@maoyan.com>
2019-03-26 08:46:21 +0800

    update

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,10 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    count: 0
+  }
+
+  handleAdd = () => {
+    const { count } = this.state
+    this.state.count = count + 1
+    console.log('加法：', this.state.count)
+  }
+
+  handleSubtract = () => {
+    const { count } = this.state
+    this.setState({
+      count: count - 1
+    })
+    console.log('减法：', this.state.count)
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +37,12 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <div>
+          <button onClick={this.handleAdd}>+</button>&nbsp;&nbsp;
+          <span>{this.state.count}</span>&nbsp;&nbsp;
+          <button onClick={this.handleSubtract}>-</button>
+        </div>
       </div>
     );
   }
