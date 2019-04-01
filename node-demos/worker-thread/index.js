d077aa83ca40d1a13e5d3b17640dda65a432ed10
yongxin.mo <yongxin.mo@shopee.com>
2019-04-01 18:51:49 +0800

    add: node worker thread demo

---
+++ node-demos/worker-thread/index.js
@@ -0,0 +1,30 @@
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads')
+
+let currentVal = 0
+let intervals = [100, 1000, 500]
+
+function counter(id, i) {
+	console.log('[', id, ']', i)
+	return i
+}
+
+if (isMainThread) {
+	console.log('this is the main thread')
+	for (let i = 0; i < 2; i++) {
+		let w = new Worker(__filename, { workerData: i })
+	}
+
+	setInterval(
+    id => currentVal = counter(id, currentVal + 1),
+    intervals[2],
+    'MainThread'
+  )
+} else {
+	console.log(`this isn't`)
+
+	setInterval(
+    id => currentVal = counter(id, currentVal + 1),
+    intervals[workerData],
+    workerData
+  )
+}
