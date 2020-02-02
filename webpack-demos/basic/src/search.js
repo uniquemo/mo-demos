21ce6daaf53404c054706920604de71db41748da
unique.mo <myx3901051@qq.com>
2020-02-02 16:58:05 +0800

    feat: 实践 treeshaking to webpack demo

--- webpack-demos/basic/src/search.js
+++ webpack-demos/basic/src/search.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import logo from './images/wechat.png'
+import { a } from './tree-shaking'
+
 import './search.less'
 
 class Search extends React.Component {
   render () {
+    console.log(a())
+
     return (
       <div className='search-text'>
         Search Text
