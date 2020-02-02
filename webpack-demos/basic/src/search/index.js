2138fd40aa42b0631cbc83a2345c5c7230962668
unique.mo <myx3901051@qq.com>
2020-02-02 21:49:30 +0800

    feat: add 代码分割和动态import to webpack demo

--- webpack-demos/basic/src/search/index.js
+++ webpack-demos/basic/src/search/index.js
@@ -7,13 +7,30 @@ import '../../common'
 import './search.less'
 
 class Search extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      Text: null
+    }
+  }
+
+  loadComponent () {
+    import('./text.js').then((Text) => {
+      this.setState({ Text: Text.default })
+    })
+  }
+
   render () {
     console.log(a())
+    const { Text } = this.state
 
     return (
       <div className='search-text'>
+        {
+          Text ? <Text /> : null
+        }
         Search Text
-        <img src={logo} />
+        <img src={logo} onClick={this.loadComponent.bind(this)} />
       </div>
     )
   }
