a7c389e185e5e6ec75b3796f5784d1f5f89fa314
unique.mo <myx3901051@qq.com>
2020-04-11 20:00:01 +0800

    feat(node-demo): add node性能调优

--- node-demos/geekbang/like-geektime/download/index.js
+++ node-demos/geekbang/like-geektime/download/index.js
@@ -9,11 +9,23 @@ app.use(
   static(__dirname + '/source/')
 )
 
+const buffer = fs.readFileSync(__dirname + '/source/index.html')
+
+// const leak = []
+
 app.use(
   mount('/', async (ctx) => {
-    ctx.body = fs.readFileSync(__dirname + '/source/index.htm', 'utf-8')
+    // leak.push(str)
+    // const str = fs.readFileSync(__dirname + '/source/index.html', 'utf-8')
+    // ctx.body = str
+
+    ctx.status = 200
+    ctx.type = 'html'
+    ctx.body = buffer
+
+    // ctx.body = fs.readFileSync(__dirname + '/source/index.html', 'utf-8')
   })
 )
 
-// app.listen(4000);
+// app.listen(3000)
 module.exports = app
