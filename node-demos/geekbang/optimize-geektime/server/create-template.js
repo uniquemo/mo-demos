a1e48fdc013495dcab2803acd70f02aceec6ea95
unique.mo <myx3901051@qq.com>
2020-04-15 22:32:20 +0800

    feat(node-demo): add optimize-like-geektime

---
+++ node-demos/geekbang/optimize-geektime/server/create-template.js
@@ -0,0 +1,15 @@
+const vm = require('vm')
+const templateContext = vm.createContext({})
+
+function createTemplate(templateContent) {
+  return vm.runInContext(
+    `(function (data) {
+      with (data) {
+        return \`${templateContent}\`
+      }
+    })`,
+    templateContext
+  )
+}
+
+module.exports = createTemplate
