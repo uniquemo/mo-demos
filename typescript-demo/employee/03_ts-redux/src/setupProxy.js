64c0428b9aae4aa57ffed47bd8c591bcdac49347
unique.mo <myx3901051@qq.com>
2020-04-16 12:27:12 +0800

    feat(typescript-demo): add ts-express demo

--- typescript-demo/employee/03_ts-redux/src/setupProxy.js
+++ typescript-demo/employee/03_ts-redux/src/setupProxy.js
@@ -1,10 +1,17 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
+    // 使用npm script`npm run server`启动mock服务
+    // app.use(proxy('/api', {
+    //   target: 'http://localhost:4000',
+    //   pathRewrite(path) {
+    //     return path.replace(/^\/api([^?]+)/, '$1.json');
+    //   }
+    // }));
+
+
+    // 使用04_ts-express启动的服务
     app.use(proxy('/api', {
-      target: 'http://localhost:4000',
-      pathRewrite(path) {
-        return path.replace(/^\/api([^?]+)/, '$1.json');
-      }
-    }));
+      target: 'http://localhost:4001'
+    }))
 };
---
+++ typescript-demo/employee/04_ts-express/README.md
@@ -0,0 +1,9 @@
+
+## 服务器启动步骤（04_ts-express）
+需要先启动mysql数据库
+
+1. npm run build
+2. npm start(npm run watch)
+
+## 客户端启动（03_ts-redux）
+1. yarn start
